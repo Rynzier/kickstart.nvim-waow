2ce72b3389ef4c8a7474ab52925c58abcef0b27d
Rynzier <rynziez@gmail.com>
2024-10-01 18:50:06 -0400

    noice setup

---
+++ lua/custom/plugins/noice.lua
@@ -0,0 +1,29 @@
+return {
+
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+
+    opts = {
+      cmdline = {
+        view = 'cmdline',
+
+        format = {
+          cmdline = {
+            pattern = '^:',
+            icon = '>',
+            lang = 'vim',
+          },
+        },
+      },
+    },
+
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      'rcarriga/nvim-notify',
+    },
+  },
+}
