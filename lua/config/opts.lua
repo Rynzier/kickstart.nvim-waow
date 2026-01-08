a127b83c0ac56aba734263a095ad38e00346f288
Rynzier <rynziez@gmail.com>
2026-01-07 22:38:44 -0500

    Gorp

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -58,10 +58,10 @@ vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 
 -- Tab settings
-vim.opt.tabstop = 4
+vim.opt.tabstop = 8
 
-vim.opt.softtabstop = 0
+vim.opt.softtabstop = 4
 
-vim.opt.expandtab = false
+vim.opt.expandtab = true
 
 vim.opt.shiftwidth = 4
